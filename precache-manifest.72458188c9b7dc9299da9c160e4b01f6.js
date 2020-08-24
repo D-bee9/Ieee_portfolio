6099a207e6465c123f093e80372e875c4f1dbab3
DristiB <dristi.biyani@gmail.com>
2020-08-25 00:51:40 +0530

    Updates

---
+++ precache-manifest.72458188c9b7dc9299da9c160e4b01f6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aff8b7c3ae60e37eca999483de40de2",
+    "url": "/Ieee_portfolio/index.html"
+  },
+  {
+    "revision": "1350eb650647d06fe885",
+    "url": "/Ieee_portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9162362e6abc6d1444f5",
+    "url": "/Ieee_portfolio/static/css/main.a6672b1a.chunk.css"
+  },
+  {
+    "revision": "1350eb650647d06fe885",
+    "url": "/Ieee_portfolio/static/js/2.c6b5f7e6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Ieee_portfolio/static/js/2.c6b5f7e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9162362e6abc6d1444f5",
+    "url": "/Ieee_portfolio/static/js/main.32317416.chunk.js"
+  },
+  {
+    "revision": "94b974740115db4fd6de",
+    "url": "/Ieee_portfolio/static/js/runtime-main.632d435b.js"
+  },
+  {
+    "revision": "c26a73b45b9a801864f13edeb0aa2cfb",
+    "url": "/Ieee_portfolio/static/media/CSS.c26a73b4.png"
+  },
+  {
+    "revision": "67e4af39f85884761f24c20d6a06da00",
+    "url": "/Ieee_portfolio/static/media/bg.67e4af39.png"
+  },
+  {
+    "revision": "664a8c00ef526bf33edf55808601d461",
+    "url": "/Ieee_portfolio/static/media/html.664a8c00.png"
+  },
+  {
+    "revision": "f58bf2428580f7fb0f60c7208bfec8b2",
+    "url": "/Ieee_portfolio/static/media/java.f58bf242.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Ieee_portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b61eb6ff168a6ccca462c1f757540121",
+    "url": "/Ieee_portfolio/static/media/pp.b61eb6ff.jpeg"
+  },
+  {
+    "revision": "89d9118583c37227a2b0699dc4f1722b",
+    "url": "/Ieee_portfolio/static/media/python.89d91185.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Ieee_portfolio/precache-manifest.72458188c9b7dc9299da9c160e4b01f6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ieee_portfolio/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
